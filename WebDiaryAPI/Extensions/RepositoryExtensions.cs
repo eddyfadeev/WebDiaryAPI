83b739b5b58c1d1d112f8bab0d0bce8a0732bfd5
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-16 21:15:37 -0700

    Added extension method to register the repository with DI

---
+++ WebDiaryAPI/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,9 @@
+﻿using WebDiaryAPI.Data.Repository;
+
+namespace WebDiaryAPI.Extensions;
+
+public static class RepositoryExtensions
+{
+    public static void AddRepository(this IServiceCollection services) =>
+        services.AddScoped<IDiaryEntriesRepository, DiaryEntriesRepository>();
+}
