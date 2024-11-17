78a1227caac9e292752597d6d3ea0b28d0a5ebba
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-11-16 20:50:50 -0700

    Added a repository interface and its concrete implementation for the Diary Entries

---
+++ WebDiaryAPI/Data/Repository/DiaryEntriesRepository.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebDiaryAPI.Models;
+
+namespace WebDiaryAPI.Data.Repository;
+
+public class DiaryEntriesRepository : IDiaryEntriesRepository
+{
+    private readonly ApplicationDbContext _context;
+    
+    public DiaryEntriesRepository(ApplicationDbContext context) => 
+        _context = context;
+    
+    public async Task<DiaryEntry?> GetByIdAsync(int id)
+    {
+        if (id <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(id), "Id must be greater than zero.");
+        }
+
+        return await _context.DiaryEntries.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<DiaryEntry>> GetAllEntriesAsync() =>
+        await _context.DiaryEntries.ToListAsync();
+
+    public async Task AddAsync(DiaryEntry? entry)
+    {
+        ArgumentNullException.ThrowIfNull(entry);
+
+        if (entry.Id != 0)
+        {
+#pragma warning disable
+            throw new ArgumentOutOfRangeException(nameof(entry.Id), "Id must be zero or not be set when adding a new entry.");
+#pragma warning restore
+        }
+        
+        await _context.DiaryEntries.AddAsync(entry);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(DiaryEntry? entry)
+    {
+        ArgumentNullException.ThrowIfNull(entry);
+
+        _context.DiaryEntries.Update(entry);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(DiaryEntry? entry)
+    {
+        ArgumentNullException.ThrowIfNull(entry);
+
+        _context.DiaryEntries.Remove(entry);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteByIdAsync(int id)
+    {
+        if (id <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(id), "Id must be greater than zero.");
+        }
+        
+        var entryToDelete = await _context.DiaryEntries.FirstOrDefaultAsync(de => de.Id == id);
+        ArgumentNullException.ThrowIfNull(entryToDelete);
+        
+        await DeleteAsync(entryToDelete);
+    }
+}
---
+++ WebDiaryAPI/Data/Repository/IDiaryEntriesRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebDiaryAPI.Models;
+
+namespace WebDiaryAPI.Data.Repository;
+
+public interface IDiaryEntriesRepository
+{
+    Task<DiaryEntry?> GetByIdAsync(int id);
+    Task<IEnumerable<DiaryEntry>> GetAllEntriesAsync();
+    Task AddAsync(DiaryEntry? entry);
+    Task UpdateAsync(DiaryEntry? entry);
+    Task DeleteAsync(DiaryEntry? entry);
+    Task DeleteByIdAsync(int id);
+}
